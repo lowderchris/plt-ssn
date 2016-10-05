ea8202cece6841b412ed8120d5ac36e37c1111a7
Chris Lowder <lowder.chris@icloud.com>
2016-10-05 16:13:27 +0100

    Streamlined figures

--- plt-ssn.py
+++ plt-ssn.py
@@ -28,7 +28,7 @@ dp = pandas.read_csv('KFprediCM.txt', sep='\s+', header=None, names=dcolsp)
 # Plot the entire range of sunspot data
 
 # Generate a figure
-f1, (ax0) = pyplot.subplots(1, figsize=[10,5])
+f1, (ax0) = pyplot.subplots(1, figsize=[6,2])
 
 # Plot full range of historical sunspot number data
 ax0.plot(d['t'], d['tsn'], 'k', label='Total')
@@ -38,9 +38,9 @@ ax0.plot(dh['t'], dh['nsn'], color=cols[2], label='Northern hemisphere')
 ax0.plot(dh['t'], dh['ssn'], color=cols[8], label='Southern hemisphere')
 
 # Tidy up the plot ranges, and label
-ax0.set_ylim([0,420])
+ax0.set_ylim([0,500])
 ax0.set_xlim([1740,2020])
-ax0.legend()
+ax0.legend(ncol=3)
 ax0.set_xlabel('Year')
 ax0.set_ylabel('Sunspot number')
 
@@ -52,7 +52,7 @@ pyplot.savefig('ssn_full.png', transparent='True')
 # Plot the range of hemispheric sunspot data
 
 # Generate a figure
-f2, (ax0) = pyplot.subplots(1, figsize=[10,5])
+f2, (ax0) = pyplot.subplots(1, figsize=[6,2])
 
 # Plot hemispheric sunspot number
 ax0.plot(dh['t'], dh['nsn'], color=cols[2], label='Northern hemisphere')
@@ -63,7 +63,7 @@ ax0.fill_between(dh['t'], dh['nsn'], dh['ssn'], where=dh['nsn']>dh['ssn'], facec
 ax0.fill_between(dh['t'], dh['nsn'], dh['ssn'], where=dh['ssn']>dh['nsn'], facecolor=cols[8], interpolate=True)
 
 # Tidy up the plot ranges, and label
-ax0.set_ylim([0,150])
+ax0.set_ylim([0,160])
 ax0.set_xlim([1991,2016])
 ax0.legend()
 ax0.set_xlabel('Year')
@@ -72,30 +72,6 @@ ax0.set_ylabel('Sunspot number')
 # Save figures to disk
 pyplot.savefig('ssn_hemi.pdf')
 pyplot.savefig('ssn_hemi.png', transparent='True')
-
-#####
-# Plot the range of hemispheric sunspot data, on a smaller scale
-
-# Generate a figure
-f3, (ax0) = pyplot.subplots(1, figsize=[5,2.5])
-
-# Plot hemispheric sunspot number
-ax0.plot(dh['t'], dh['nsn'], color=cols[2], label='Northern hemisphere')
-ax0.plot(dh['t'], dh['ssn'], color=cols[8], label='Southern hemisphere')
-
-# Fill between to indicate dominant hemisphere
-ax0.fill_between(dh['t'], dh['nsn'], dh['ssn'], where=dh['nsn']>dh['ssn'], facecolor=cols[2], interpolate=True)
-ax0.fill_between(dh['t'], dh['nsn'], dh['ssn'], where=dh['ssn']>dh['nsn'], facecolor=cols[8], interpolate=True)
-
-# Tidy up the plot ranges, and label
-ax0.set_ylim([0,150])
-ax0.set_xlim([1991,2016])
-ax0.set_xlabel('Year')
-ax0.set_ylabel('Sunspot number')
-
-# Save figures to disk
-pyplot.savefig('ssn_hemi_sm.pdf')
-pyplot.savefig('ssn_hemi_sm.png', transparent='True')
 
 #####
 # Plot the past few cycles, and an outlook on the future...
