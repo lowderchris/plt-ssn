929ce08f76361bdf05db8724291e106a9e698fb9
Chris Lowder <lowder.chris@icloud.com>
2022-04-28 13:55:00 -0600

    Updated SSN URLs and plotting styles

--- plt-ssn.py
+++ plt-ssn.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas
 import os
+from datetime import datetime
+import sunpy.coordinates
 import palettable
 import matplotlib
 from matplotlib import pyplot
@@ -8,18 +11,33 @@ from matplotlib import pyplot
 cols = palettable.colorbrewer.diverging.RdBu_11.mpl_colors
 
 # Grab current sunspot number data from the SILSO dataset
-os.system('curl -O http://sidc.oma.be/silso/DATA/SN_m_tot_V2.0.txt')
-os.system('curl -O http://sidc.oma.be/silso/DATA/SN_m_hem_V2.0.txt')
-os.system('curl -O http://sidc.oma.be/silso/FORECASTS/KFprediCM.txt')
+os.system('curl -O https://wwwbis.sidc.be/silso/DATA/SN_m_tot_V2.0.txt')
+os.system('curl -O https://wwwbis.sidc.be/silso/DATA/SN_m_hem_V2.0.txt')
+os.system('curl -O https://wwwbis.sidc.be/silso/FORECASTS/KFprediCM.txt')
 
 # Define column headers, and read in the data!
 dcols = ['yy', 'mm', 't', 'tsn', 'snsd', 'nobs', 'def']
-dcolsh = ['yy', 'mm', 't', 'tsn', 'nsn', 'ssn', 'mmsd', 'mmsdn', 'mmsds', 'nobs', 'nobsn', 'nobss']
+dcolsh = ['yy', 'mm', 't', 'tsn', 'nsn', 'ssn', 'mmsd', 'mmsdn', 'mmsds', 'nobs', 'nobsn', 'nobss', 'prov']
 dcolsp = ['yy', 'mm', 't', 'junk', 'tsn', 'unc']
 
 d = pandas.read_csv('SN_m_tot_V2.0.txt', sep='\s+',header=None, names=dcols)
 dh = pandas.read_csv('SN_m_hem_V2.0.txt', sep='\s+',header=None, names=dcolsh)
 dp = pandas.read_csv('KFprediCM.txt', sep='\s+', header=None, names=dcolsp)
+
+# Generate some time arrays
+td = []
+for i in np.arange(len(d['yy'].values)) : td.append(datetime(d['yy'].values[i], d['mm'].values[i], 1))
+
+tdh = []
+for i in np.arange(len(dh['yy'].values)) : tdh.append(datetime(dh['yy'].values[i], dh['mm'].values[i], 1))
+
+tdp = []
+for i in np.arange(len(dp['yy'].values)) : tdp.append(datetime(dp['yy'].values[i], dp['mm'].values[i], 1))
+
+# Generate an array of corresponding CR numbers
+crd = sunpy.coordinates.sun.carrington_rotation_number(td)
+crdh = sunpy.coordinates.sun.carrington_rotation_number(tdh)
+crdp = sunpy.coordinates.sun.carrington_rotation_number(tdp)
 
 ## Toggle keynote mode on and off
 keynote_figs()
@@ -30,8 +48,7 @@ lc = 'w'
 # Plot the entire range of sunspot data
 
 # Generate a figure
-#f1, (ax0) = pyplot.subplots(1, figsize=[6,2])
-f1, (ax0) = pyplot.subplots(1)
+f1, (ax0) = pyplot.subplots(1, figsize=[10,4])
 
 # Plot full range of historical sunspot number data
 ax0.plot(d['t'], d['tsn'], lc, label='Total')
@@ -42,9 +59,11 @@ ax0.plot(dh['t'], dh['ssn'], color=cols[8], label='Southern hemisphere')
 
 # Tidy up the plot ranges, and label
 ax0.set_ylim(0)
-#ax0.set_xlim([1740,2020])
+ax0a = ax0.twiny()
+ax0a.set_xlim(crd[0], crd[-1])
 ax0.legend()
-ax0.set_xlabel('Year')
+ax0.set_xlabel('Date')
+ax0a.set_xlabel('Carrington Rotation')
 ax0.set_ylabel('Sunspot number')
 
 pyplot.tight_layout()
@@ -57,8 +76,7 @@ pyplot.savefig('ssn_full.png', transparent='True')
 # Plot the range of hemispheric sunspot data
 
 # Generate a figure
-#f2, (ax0) = pyplot.subplots(1, figsize=[6,2])
-f2, (ax0) = pyplot.subplots(1)
+f1, (ax0) = pyplot.subplots(1, figsize=[10,4])
 
 # Plot hemispheric sunspot number
 ax0.plot(dh['t'], dh['nsn'], color=cols[2], label='Northern hemisphere')
@@ -70,9 +88,11 @@ ax0.fill_between(dh['t'], dh['nsn'], dh['ssn'], where=dh['ssn']>dh['nsn'], facec
 
 # Tidy up the plot ranges, and label
 ax0.set_ylim(0)
-#ax0.set_xlim([1991,2020])
+ax0a = ax0.twiny()
+ax0a.set_xlim(crdh[0], crdh[-1])
 ax0.legend()
-ax0.set_xlabel('Year')
+ax0.set_xlabel('Date')
+ax0a.set_xlabel('Carrington Rotation')
 ax0.set_ylabel('Sunspot number')
 
 pyplot.tight_layout()
@@ -85,8 +105,7 @@ pyplot.savefig('ssn_hemi.png', transparent='True')
 # Plot the past few cycles, and an outlook on the future...
 
 # Generate a figure
-#f1, (ax0) = pyplot.subplots(1, figsize=[4,3])
-f1, (ax0) = pyplot.subplots(1)
+f1, (ax0) = pyplot.subplots(1, figsize=[10,4])
 
 # Plot full range of historical sunspot number data
 ax0.plot(d['t'], d['tsn'], lc, label='Total')
@@ -95,11 +114,14 @@ ax0.fill_between(dp['t'], dp['tsn']+dp['unc'], dp['tsn']-dp['unc'], color=cols[6
 ax0.plot(dp['t'], dp['tsn'], color=cols[8], label='KF CM Pred.')
 
 # Tidy up the plot ranges, and label
-ax0.set_ylim([0,275])
-ax0.set_xlim([1995,2023])
+ax0a = ax0.twiny()
+ax0a.set_xlim(crdh[0], crdh[-1])
+
 ax0.legend()
-ax0.set_xlabel('Year')
+ax0.set_xlabel('Date')
 ax0.set_ylabel('Sunspot number')
+
+pyplot.tight_layout()
 
 # Save figures to disk
 pyplot.savefig('ssn_pred.pdf')
